62f265ef871dcc4bdf1fb577419ffd8026483c8d
agutell <alex.gutell@gmail.com>
2024-11-15 16:55:49 +0100

    intermediate

--- data/measurement_models.py
+++ data/measurement_models.py
@@ -104,4 +104,59 @@ class LinearBlurring(object):
         pass
 
 
+class NonLinearBlurring(object):
+
+    def __init__(self, noise_model="gaussian", sigma=1.):
+        self.sigma = sigma
+        if (noise_model != "gaussian") and (noise_model != "poisson"):
+            print(f"Noise model {noise_model} not implemented! Use 'gaussian' or 'poisson'.")
+            return ValueError 
+        self.noise_model = noise_model
+
+    def generate_blur(self):
+        # NOTE: This code is copy-pasted from https://github.com/VinAIResearch/blur-kernel-space-exploring/blob/main/generate_blur.py
+        import argparse
+        #import cv2
+        #import numpy as np
+        #import os.path as osp
+        import torch
+        #import utils.util as util
+        import yaml
         
+        # TODO: Make import work... 
+        from models.kernel_encoding.kernel_wizard import KernelWizard
+        
+        #device = torch.device("cuda")
+
+        parser = argparse.ArgumentParser(description="Kernel extractor testing")
+
+        #parser.add_argument("--image_path", action="store", help="image path", type=str, required=True)
+        parser.add_argument("--yml_path", action="store", help="yml path", type=str, required=True)
+        #parser.add_argument("--save_path", action="store", help="save path", type=str, default=".")
+        #parser.add_argument("--num_samples", action="store", help="number of samples", type=int, default=1)
+
+        args = parser.parse_args()
+
+        #image_path = args.image_path
+        yml_path = args.yml_path
+        #num_samples = args.num_samples
+
+        # Initializing mode
+        with open(yml_path, "r") as f:
+            opt = yaml.load(f)["KernelWizard"]
+            model_path = opt["pretrained"]
+        model = KernelWizard(opt)
+        model.eval()
+        model.load_state_dict(torch.load(model_path))
+        model = model.to(device)
+
+        #HQ = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB) / 255.0
+        #HQ = np.transpose(HQ, (2, 0, 1))
+        #HQ_tensor = torch.Tensor(HQ).unsqueeze(0).to(device).cuda()
+
+        with torch.no_grad():
+            kernel = torch.randn((1, 512, 2, 2)).cuda() * 1.2
+            LQ_tensor = model.adaptKernel(HQ_tensor, kernel)
+
+
+
